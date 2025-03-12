baf11132e9aa1438d7972c89a1526df9da58579e
Dilna <dilnavincent@gmail.com>
2025-03-12 06:33:07 +0530

    Updated Favicon,home and search

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
